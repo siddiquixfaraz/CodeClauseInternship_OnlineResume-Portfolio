a054bcb7a6bb78ce4df8e2ef882c07da7e7304bc
siddiquixfaraz <109731664+siddiquixfaraz@users.noreply.github.com>
2023-09-01 22:54:58 +0530

    responsive header commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,10 @@
 import '../styles/Portfolio.css';
-// import img1 from 'public/images/project-ss-1.png'
-// import img2 from 'public/images/project-ss-2.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLink, faPlay,faCodeFork,faSquareGithub} from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 
-export const Card = ({img,gitlink,livelink}) => {
+export const Card = ({img,gitlink,livelink,title,des}) => {
   return (
     <div className="cards">
             <div className='border-div'></div>
@@ -14,8 +12,8 @@ export const Card = ({img,gitlink,livelink}) => {
                     <img src={img} alt="" />
                 </div>
                 <div className="cards-content">
-                <span>Beat Buddy</span>
-                <span>Music website Made in reactjs</span>
+                <span>{title}</span>
+                <span>{des}</span>
                 <div className="content-logo">
                 <a href={gitlink} target="_blank"><img src="https://img.favpng.com/4/20/21/github-computer-icons-icon-design-png-favpng-KaDTwuHWgG8D1qn47QW2hK7Gj.jpg" alt="" /></a>
                 <a href={livelink} target="_blank"><FontAwesomeIcon icon={faLink} /></a>
@@ -34,11 +32,27 @@ const Portfolio = () => {
             <h2>Portfolio</h2>
         </div>
         <div className="projects">
-            <Card img="./images/project-ss-1.png" gitlink="https://github.com/siddiquixfaraz/ReactPortfolioXfaraz" livelink="https://faraz-portfolio.vercel.app/"/>
-            <Card img="./images/project-ss-2.png" gitlink="https://github.com/siddiquixfaraz/ReactjsBeatBuddy" livelink="https://reactjs-beat-buddy.vercel.app/"/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card 
+            img="./images/project-ss-1.png" 
+            gitlink="https://github.com/siddiquixfaraz/ReactPortfolioXfaraz" 
+            livelink="https://faraz-portfolio.vercel.app/" 
+            title="Personal Portfolio"
+            des="Personal Portfolio made in reactjs "/>
+            
+            <Card 
+            img="./images/project-ss-2.png" 
+            gitlink="https://github.com/siddiquixfaraz/ReactjsBeatBuddy" 
+            livelink="https://reactjs-beat-buddy.vercel.app/" 
+            title="Beat Buddy"
+            des="Music Website made in reactjs "/>
+            <Card
+            img="./images/project-ss-3.png"
+            gitlink="https://github.com/siddiquixfaraz/blogapp"
+            livelink="https://postblog-ten.vercel.app/"
+            title="Blog Post"
+            des="Blog Website made with MERN Stack"
+            />
+            
         </div>
     </div>
     </section>
